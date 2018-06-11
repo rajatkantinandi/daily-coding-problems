882609c4bbdd5132c40be8ef87d7161dc949eaf1
Rajat <rajatkantinandi@github.com>
2018-06-11 12:24:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Daily Coding problems in C
+
+## One problem per day starting from May 28th, 2018
+
+### List of problems
+
+- [x] 1.  Given a list of numbers, return whether any two sums to k. @28.05.18
---
+++ src/problem1.c
@@ -0,0 +1,46 @@
+/* 
+Given a list of numbers, return whether any two sums to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+Bonus: Can you do this in one pass? */
+#include <stdio.h>
+#include <stdbool.h>
+bool match(int *arr, int k, int n);
+int main()
+{
+  int k = 0, n = 0;
+  printf("Enter the number of elements of the array:");
+  scanf("%d", &n);
+  int arr[n];
+  printf("Enter the elements of the array: ");
+  for (int i = 0; i < n; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+  printf("Enter the value of K: ");
+  scanf("%d", &k);
+  if (k > 1 && n > 0)
+  {
+    if (match(arr, k, n))
+    {
+      printf("The array has 2 elements summing up to K=%d\n", k);
+    }
+    else
+    {
+      printf("The array doesn't contain 2 elements summing up to K=%d\n", k);
+    }
+  }
+}
+bool match(int *arr, int k, int n)
+{
+  for (int i = 0; i < n - 1; i++)
+  {
+    for (int j = i + 1; j < n; j++)
+    {
+      if (arr[i] + arr[j] == k)
+      {
+        return true;
+      }
+    }
+  }
+  return false;
+}
